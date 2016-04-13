cb8546bf0557ed95401585cf7cf41919e7d1d7f0
shupac800 <davidshuman@gmail.com>
2016-04-13 18:31:24 -0500

    SSTest doesn't create new instances of shapes

--- SSTest/UnitTest1.cs
+++ SSTest/UnitTest1.cs
@@ -10,42 +10,37 @@ namespace SSTest
         [TestMethod]
         public void CircleTest()
         {
-            Circle circle = new Circle();
             double radius = 1;
-            Assert.AreEqual(Circle.pi, circle.area(radius));
-            Assert.AreEqual(Circle.pi * 2 * radius, circle.diameter(radius));
+            Assert.AreEqual(Circle.pi, Circle.area(radius));
+            Assert.AreEqual(Circle.pi * 2 * radius, Circle.diameter(radius));
         }
 
         [TestMethod]
         public void SquareTest()
         {
-            Square square = new SharpShapes.Square();
-            Assert.AreEqual(9, square.area(3));
-            Assert.AreEqual(12, square.perimeter(3));
+            Assert.AreEqual(9, Square.area(3));
+            Assert.AreEqual(12, Square.perimeter(3));
         }
 
         [TestMethod]
         public void RhombusTest()
         {
-            Rhombus rhombus = new SharpShapes.Rhombus();
-            Assert.AreEqual(16, rhombus.area(4));
-            Assert.AreEqual(20, rhombus.perimeter(5));
+            Assert.AreEqual(16, Rhombus.area(4));
+            Assert.AreEqual(20, Rhombus.perimeter(5));
         }
 
         [TestMethod]
         public void CylinderTest()
         {
-            Cylinder cylinder = new Cylinder();
-            Assert.AreEqual(Cylinder.pi * 1 * 1 * 3, cylinder.volume(1, 3));
-            Assert.AreEqual( (2 * Cylinder.pi * 1 * 3) + 2 * Cylinder.pi * 1 * 1, cylinder.area(1, 3) );
+            Assert.AreEqual(Cylinder.pi * 1 * 1 * 3, Cylinder.volume(1, 3));
+            Assert.AreEqual( (2 * Cylinder.pi * 1 * 3) + 2 * Cylinder.pi * 1 * 1, Cylinder.area(1, 3) );
         }
 
         [TestMethod]
         public void CubeTest()
         {
-            Cube cube = new SharpShapes.Cube();
-            Assert.AreEqual(24, cube.area(2));
-            Assert.AreEqual(8, cube.volume(2));
+            Assert.AreEqual(24, Cube.area(2));
+            Assert.AreEqual(8, Cube.volume(2));
         }
     }
 }
