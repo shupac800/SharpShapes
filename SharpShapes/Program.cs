62077234584da35a22f04a8b7ebf5a343042da27
shupac800 <davidshuman@gmail.com>
2016-04-14 11:11:08 -0500

    refactored to put I/O in program.cs; added Sphere.cs

--- SharpShapes/Program.cs
+++ SharpShapes/Program.cs
@@ -10,7 +10,7 @@ namespace SharpShapes
     {
         static void Main(string[] args)
         {
-            List<string> Shapes = new List<string> { "circle", "square", "rhombus", "cylinder", "cube" };
+            List<string> Shapes = new List<string> { "circle", "square", "rhombus", "cylinder", "cube", "sphere" };
             Console.WriteLine("\nGREETINGS PROFESSOR FALKEN\n");
             Console.WriteLine("Select a Shape:");
             int i = 1;
@@ -24,19 +24,49 @@ namespace SharpShapes
             switch(choice)
             {
                 case 0:
-                    Circle.inputAndCompute();
+                    // circle
+                    Console.WriteLine("Enter radius");
+                    var circle_radius = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Area is {0}", Circle.area(circle_radius));
+                    Console.WriteLine("Diameter is {0}", Circle.diameter(circle_radius));
                     break;
                 case 1:
-                    Square.inputAndCompute();
+                    // square
+                    Console.WriteLine("Enter side");
+                    var square_side = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Area is {0}", Square.area(square_side));
+                    Console.WriteLine("Perimeter is {0}", Square.perimeter(square_side));
                     break;
                 case 2:
-                    Rhombus.inputAndCompute();
+                    // rhombus
+                    Console.WriteLine("Enter side");
+                    var rhombus_side = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Area is {0}", Rhombus.area(rhombus_side));
+                    Console.WriteLine("Perimeter is {0}", Rhombus.perimeter(rhombus_side));
                     break;
                 case 3:
-                    Cylinder.inputAndCompute();
+                    // cylinder
+                    Console.WriteLine("Enter radius, height");
+                    string cyl_values = Console.ReadLine();
+                    string[] cyl_arguments = cyl_values.Split(',');
+                    double cyl_radius = double.Parse(cyl_arguments[0]);
+                    double cyl_height = double.Parse(cyl_arguments[1]);
+                    Console.WriteLine("Area is {0}", Cylinder.area(cyl_radius, cyl_height));
+                    Console.WriteLine("Volume is {0}", Cylinder.volume(cyl_radius, cyl_height));
                     break;
                 case 4:
-                    Cube.inputAndCompute();
+                    // cube
+                    Console.WriteLine("Enter side");
+                    var cube_side = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Area is {0}", Cube.area(cube_side));
+                    Console.WriteLine("Volume is {0}", Cube.volume(cube_side));
+                    break;
+                case 5:
+                    // sphere
+                    Console.WriteLine("Enter radius");
+                    var sphere_radius = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Area is {0}", Sphere.area(sphere_radius));
+                    Console.WriteLine("Volume is {0}", Sphere.volume(sphere_radius));
                     break;
                 default:
                     Console.WriteLine("Invalid Selection");
